649df87684c1d1a5db1e5d8eb2abb1d965a21325
sauban <musaddiq.sauban@gmail.com>
2020-04-30 22:25:02 -0700

    Mcphee's Astar

--- search.py
+++ search.py
@@ -62,7 +62,22 @@ class SearchProblem:
         The sequence must be composed of legal moves.
         """
         util.raiseNotDefined()
+        
+class pnode:
+    def __init__(self, nstate, acts, cost):
+        self.state = nstate
+        self.acts = acts
+        self.cost = cost
 
+
+    def getstate(self):
+        return self.state
+
+    def getacts(self):
+        return self.acts
+
+    def getcost(self):
+        return self.cost
 
 def tinyMazeSearch(problem):
     """
@@ -262,6 +277,95 @@ def aStarSearch(problem, heuristic=nullHeuristic):
 
     path.reverse()
     return path
+
+def calcstar(snode, goal, walls):
+    sQueue = util.PriorityQueue()
+    cnode = pnode(snode.getstate(), [], 0)
+    aset = {cnode}
+    fringelist = [(cnode.getstate()[0]+1,cnode.getstate()[1]), (cnode.getstate()[0]-1,cnode.getstate()[1]),
+    (cnode.getstate()[0],cnode.getstate()[1]-1), (cnode.getstate()[0],cnode.getstate()[1]+1)]
+    dir = ["East", "West", "South", "North"]
+    for i in range(0, len(fringelist)):
+        aset.add(fringelist[i][0])
+        if fringelist[i] not in walls:
+            wlist = cnode.getacts()[:]
+            wlist.append(dir[i])
+            newnode=pnode(fringelist[i], wlist, cnode.getcost()+1)
+            sQueue.push(newnode, newnode.getcost())
+    while cnode.getstate() != goal:
+        if sQueue.isEmpty():
+            return none
+        cnode = sQueue.pop()
+        fringelist = [(cnode.getstate()[0]+1,cnode.getstate()[1]), (cnode.getstate()[0]-1,cnode.getstate()[1]),
+        (cnode.getstate()[0],cnode.getstate()[1]-1), (cnode.getstate()[0],cnode.getstate()[1]+1)]
+        dir = ["East", "West", "South", "North"]
+        for i in range(0, len(fringelist)):
+            if fringelist[i] not in aset:
+                aset.add(fringelist[i])
+                if fringelist[i] not in walls:
+                    wlist = cnode.getacts()[:]
+                    wlist.append(dir[i])
+                    newnode=pnode(fringelist[i], wlist, cnode.getcost()+1)
+                    hval = util.manhattanDistance(newnode.getstate(), goal)
+                    sQueue.push(newnode, newnode.getcost()+hval)
+    return cnode.getacts()
+
+
+
+def astar2(problem, view_range=2):
+    goal= problem.goal
+    cstate = problem.startState
+    wlist = []
+    knownwalls = []
+    cost = 0
+    walls = problem.walls
+    for i in range(0, walls.height):
+        knownwalls.append((0, i))
+        knownwalls.append((walls.width, i))
+    for i in range(0, walls.width):
+        knownwalls.append((i, 0))
+        knownwalls.append((i, walls.height))
+    cnode = pnode(cstate, wlist, 0)
+    #hval=heuristic(cnode.getstate(), problem)
+    sQueue = util.PriorityQueue()
+    directions = calcstar(cnode, goal, knownwalls)
+    while not problem.isGoalState(cnode.getstate()):
+        pos = cnode.getstate()
+        for i in range(max(1,pos[0]-view_range),min(walls.width,pos[0]+view_range)):
+            for j in range(max(1,pos[1]-view_range),min(walls.height,pos[1]+view_range)):
+                if walls[i][j]==True:
+                    knownwalls.append((i,j))
+        successors = problem.getSuccessors(cnode.getstate())
+#        qlist = []
+#        for successor in successors: #adding perceivable walls
+#            qlist.append(successor[0])
+#        if ((cnode.getstate()[0],cnode.getstate()[1]+1) not in qlist) and ((cnode.getstate()[0],cnode.getstate()[1]+1) not in knownwalls):
+#            knownwalls.append((cnode.getstate()[0],cnode.getstate()[1]+1))
+#        if ((cnode.getstate()[0],cnode.getstate()[1]-1) not in qlist) and ((cnode.getstate()[0],cnode.getstate()[1]-1) not in knownwalls):
+#            knownwalls.append((cnode.getstate()[0],cnode.getstate()[1]-1))
+#        if ((cnode.getstate()[0]+1,cnode.getstate()[1]) not in qlist) and ((cnode.getstate()[0]+1,cnode.getstate()[1]) not in knownwalls):
+#            knownwalls.append((cnode.getstate()[0]+1,cnode.getstate()[1]))
+#        if ((cnode.getstate()[0]-1,cnode.getstate()[1]) not in qlist) and ((cnode.getstate()[0]-1,cnode.getstate()[1]) not in knownwalls):
+#            knownwalls.append((cnode.getstate()[0]-1,cnode.getstate()[1]))
+        direction = directions.pop(0)
+        t = 0
+        if direction=="North":
+            newloc = (cnode.getstate()[0], cnode.getstate()[1]+1)
+        if direction=="West":
+            newloc = (cnode.getstate()[0]-1, cnode.getstate()[1])
+        if direction=="South":
+            newloc = (cnode.getstate()[0], cnode.getstate()[1]-1)
+        if direction=="East":
+            newloc = (cnode.getstate()[0]+1, cnode.getstate()[1])
+        if newloc in knownwalls:
+            directions = calcstar(cnode, goal, knownwalls)
+        else:
+            wlist.append(direction)
+            cost = cost+1
+            cnode=pnode(newloc, wlist, cost)
+
+    return cnode.getacts()
+
 
 
 def dStarLiteSearch(problem, heuristic=nullHeuristic):
