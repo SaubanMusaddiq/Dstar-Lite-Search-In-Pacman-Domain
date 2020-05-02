f139b2211c70f4695e00feb8373a02443cf3bef7
sauban <musaddiq.sauban@gmail.com>
2020-05-02 00:01:54 -0700

    Refactored Searchagents

--- searchAgents.py
+++ searchAgents.py
@@ -73,20 +73,13 @@ class SearchAgent(Agent):
     Note: You should NOT change any code in SearchAgent
     """
 
-    def __init__(self, fn='depthFirstSearch', prob='PositionSearchProblem', heuristic='nullHeuristic', visibility=2):
+    def __init__(self, fn='depthFirstSearch', prob='PositionSearchProblem', heuristic='nullHeuristic'):
         # Warning: some advanced Python magic is employed below to find the right functions and problems
 
         # Get the search function from the name and heuristic
-        if(isinstance(visibility, str)):
-            if(visibility.isdigit()):
-                visibility = int(visibility)
-            else:
-                visibility = 2
-
         if fn not in dir(search):
             raise AttributeError, fn + ' is not a search function in search.py.'
         func = getattr(search, fn)
-        print(visibility)
         if 'heuristic' not in func.func_code.co_varnames:
             print('[SearchAgent] using function ' + fn)
             self.searchFunction = func
@@ -99,8 +92,7 @@ class SearchAgent(Agent):
                 raise AttributeError, heuristic + ' is not a function in searchAgents.py or search.py.'
             print('[SearchAgent] using function %s and heuristic %s' % (fn, heuristic))
             # Note: this bit of Python trickery combines the search algorithm and the heuristic
-            print(visibility)
-            self.searchFunction = lambda x: func(x, heuristic=heur, visibility=visibility)
+            self.searchFunction = lambda x: func(x, heuristic=heur)
 
         # Get the search problem type from the name
         if prob not in globals().keys() or not prob.endswith('Problem'):
@@ -140,6 +132,7 @@ class SearchAgent(Agent):
             return self.actions[i]
         else:
             return Directions.STOP
+
 
 
 
@@ -237,18 +230,91 @@ class PositionSearchProblem(search.SearchProblem):
             cost += self.costFn((x,y))
         return cost
 
-
-class LimitedSearchAgent(SearchAgent):
+class LimitedSearchAgent(Agent):
     """
-    An agent for position search with a cost function that penalizes being in
-    positions on the West side of the board.
+    This very general search agent finds a path using a supplied search
+    algorithm for a supplied search problem, then returns actions to follow that
+    path.
 
-    The cost function for stepping into a position (x,y) is 1/2^x.
+    As a default, this agent runs DFS on a PositionSearchProblem to find
+    location (1,1)
+
+    Options for fn include:
+      depthFirstSearch or dfs
+      breadthFirstSearch or bfs
+
+
+    Note: You should NOT change any code in SearchAgent
     """
-    def __init__(self):
-        SearchAgent.__init__(self)
-        self.searchFunction = lambda prob: search.dStarLiteSearch(prob)
-        self.searchType = LimitedPositionSearchProblem
+
+    def __init__(self, fn='dStarLiteSearch', prob='LimitedPositionSearchProblem', heuristic='nullHeuristic', visibility=2):
+        # Warning: some advanced Python magic is employed below to find the right functions and problems
+
+        # Get the search function from the name and heuristic
+        if(isinstance(visibility, str)):
+            if(visibility.isdigit()):
+                visibility = int(visibility)
+            else:
+                visibility = 2
+
+        if fn not in dir(search):
+            raise AttributeError, fn + ' is not a search function in search.py.'
+        func = getattr(search, fn)
+        print(visibility)
+        if 'heuristic' not in func.func_code.co_varnames:
+            print('[SearchAgent] using function ' + fn)
+            self.searchFunction = func
+        else:
+            if heuristic in globals().keys():
+                heur = globals()[heuristic]
+            elif heuristic in dir(search):
+                heur = getattr(search, heuristic)
+            else:
+                raise AttributeError, heuristic + ' is not a function in searchAgents.py or search.py.'
+            print('[SearchAgent] using function %s and heuristic %s' % (fn, heuristic))
+            # Note: this bit of Python trickery combines the search algorithm and the heuristic
+            print(visibility)
+            self.searchFunction = lambda x: func(x, heuristic=heur, visibility=visibility)
+
+        # Get the search problem type from the name
+        if prob not in globals().keys() or not prob.endswith('Problem'):
+            raise AttributeError, prob + ' is not a search problem type in SearchAgents.py.'
+        self.searchType = globals()[prob]
+        print('[SearchAgent] using problem type ' + prob)
+
+    def registerInitialState(self, state):
+        """
+        This is the first time that the agent sees the layout of the game
+        board. Here, we choose a path to the goal. In this phase, the agent
+        should compute the path to the goal and store it in a local variable.
+        All of the work is done in this method!
+
+        state: a GameState object (pacman.py)
+        """
+        if self.searchFunction == None: raise Exception, "No search function provided for SearchAgent"
+        starttime = time.time()
+        problem = self.searchType(state) # Makes a new search problem
+        self.actions  = self.searchFunction(problem) # Find a path
+        totalCost = problem.getCostOfActions(self.actions)
+        print('Path found with total cost of %d in %.1f seconds' % (totalCost, time.time() - starttime))
+        if '_expanded' in dir(problem): print('Search nodes expanded: %d' % problem._expanded)
+
+    def getAction(self, state):
+        """
+        Returns the next action in the path chosen earlier (in
+        registerInitialState).  Return Directions.STOP if there is no further
+        action to take.
+
+        state: a GameState object (pacman.py)
+        """
+        if 'actionIndex' not in dir(self): self.actionIndex = 0
+        i = self.actionIndex
+        self.actionIndex += 1
+        if i < len(self.actions):
+            return self.actions[i]
+        else:
+            return Directions.STOP
+
 
 
 class LimitedPositionSearchProblem(search.SearchProblem):
